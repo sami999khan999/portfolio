620605b2647ae6d0dbdbc258042c87b7953430f1
Sami Khan <sami999khan999@gmail.com>
2024-03-27 04:13:20 +0600

    hero section 80% done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,12 +1,45 @@
 import { RiArrowDownSLine } from "react-icons/ri";
+import { Download, Send } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Socials from "./Socials";
 
 const Hero = () => {
   return (
     <section className="py-12 xl:py-12 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
       <div className="container mx-auto ">
-        <div className="flex justify-between gap-x-8  ">
-          <div>text</div>
-          <div>image</div>
+        <div className="flex justify-between gap-x-8">
+          <div className="flex flex-col max-w-[600px] justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold md-4 text-primary tracking-[4px]">
+              Web Developer
+            </div>
+
+            <h1 className="h1 mb-4">Hello, my name is Munum Khan Sami</h1>
+
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              Brief description with insights into myself, my vocational
+              journey, and what I engage in professionally.
+            </p>
+
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0">
+              <Link href="/contact">
+                <Button className="gap-x-2">
+                  Contact Me <Send size={18} />
+                </Button>
+              </Link>
+
+              <Button variant="secondary" className="gap-x-2">
+                Download CV <Download size={18} />
+              </Button>
+            </div>
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0 mt-12"
+              iconsStyles="text-foreground text-[22px] hover:text-primary transition-all"
+            />
+          </div>
+          <div className="hidden xl:flex relative">
+            <div className="bg-hero_shape2_light w-[500px] h-[500px]"></div>
+          </div>
         </div>
 
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,5 +1,49 @@
-const Socials = () => {
-  return <div>Socials</div>;
+"use client";
+
+import Link from "next/link";
+import {
+  RiYoutubeFill,
+  RiLinkedinFill,
+  RiGithubFill,
+  RiFacebookBoxFill,
+  RiTwitterFill,
+} from "react-icons/ri";
+
+const icons = [
+  {
+    path: "/",
+    name: <RiLinkedinFill />,
+  },
+  {
+    path: "/",
+    name: <RiGithubFill />,
+  },
+  {
+    path: "/",
+    name: <RiYoutubeFill />,
+  },
+  {
+    path: "/",
+    name: <RiFacebookBoxFill />,
+  },
+  {
+    path: "/",
+    name: <RiTwitterFill />,
+  },
+];
+
+const Socials = ({ containerStyles, iconsStyles }) => {
+  return (
+    <div className={`${containerStyles}`}>
+      {icons.map((icon) => {
+        return (
+          <Link href={icon.path}>
+            <div className={`${iconsStyles}`}>{icon.name}</div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Socials;
